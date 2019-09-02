4114cb7688c6817d7598b8ec3fa2ef0ed86a3c47
Jack hosking <jackhosking2@outlook.com>
2019-09-02 17:17:07 +1200

    Sum of list and map, using sum() and sumBy()

--- kotlin_examples/Sum.kt
+++ kotlin_examples/Sum.kt
@@ -5,5 +5,32 @@ fun sum(a: Int, b: Int): Int {
 }
 
 fun main(args: Array<String>) {
-    println(sum(5, 6))
+    println("5 + 6 = : " + sum(5, 6))
+
+    //Can also sum a list
+    val personList = listOf(Person("Jack", "Hosking", 22),
+            Person("Jack", "Hosking", 22),
+            Person("Jack", "Hosking", 22)
+    )
+
+    //Example using map and sum()
+    val totalAgeInPersonList1: Int = personList.map { it.age }.sum()
+    println("Using Sum(): $totalAgeInPersonList1")
+
+    //Example using sumBy()
+    val totalAgeInPersonList2: Int = personList.sumBy { it.age }
+    println("Using sumBy(): $totalAgeInPersonList2")
+
+    //And a map
+    val personMap = mapOf(
+            1 to Person("Jack", "Hosking", 22),
+            2 to Person("Jack", "Hosking", 22),
+            3 to Person("Jack", "Hosking", 22)
+    )
+
+    val totalAgeInPersonMap1: Int = personMap.map { it.value.age }.sum()
+    println("Using sum(): $totalAgeInPersonMap1")
+
+    val totalAgeInPersonMap2: Int = personMap.map { it.value }.sumBy { it.age }
+    println("Using sumBy(): $totalAgeInPersonMap2")
 }
