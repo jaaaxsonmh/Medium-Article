66042ddf730e72fef3a54f50be4597ff568f990d
Jack hosking <jackhosking2@outlook.com>
2019-09-02 16:13:36 +1200

    Person Class differences Kotlin vs Java

---
+++ PersonJava.java
@@ -0,0 +1,37 @@
+import java.util.Date;
+
+public class PersonJava {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public PersonJava(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
