80838fa721e6d6bef3f5978bd23a9ef24e1b7d2e
Jack hosking <jackhosking2@outlook.com>
2019-09-02 17:07:23 +1200

    Integer Sum method

--- java_examples/Sum.java
+++ java_examples/Sum.java
@@ -4,6 +4,9 @@ public class Sum {
 
     public static void main(String args[]) {
         System.out.println(sum(5, 6));
+
+        // We can also do this using Java.lang.Integer.sum() method
+        System.out.println(Integer.sum(10, 15));
     }
 
     // But this is static, to sum with non-static need to create object.
