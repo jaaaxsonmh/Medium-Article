b11355b4e2d446748be57ad08f264291613f3543
Jack hosking <jackhosking2@outlook.com>
2019-09-03 22:34:35 +1200

    few more examples of if statements and "ternary"

--- kotlin_examples/control/ifexpression.kt
+++ kotlin_examples/control/ifexpression.kt
@@ -6,13 +6,26 @@ fun smaller(a: Int, b: Int) = if (a > b) b else a
 
 fun main(args: Array<String>) {
 
-    val number = (1..1000).shuffled().first()
+    val number = (1..10).shuffled().first()
 
-    if(number < 500) {
-        println("$number is less than 500")
+    if (number < 5) {
+        println("$number is less than 5")
     } else {
-        println("$number is greater than 500")
+        println("$number is greater than 5")
     }
+
+    // Used as expression as returns value
+    val result = if (number > 5) {
+        "$number is greater than 5"
+    } else {
+        "$number is less than 5"
+    }
+
+    println("Result is that: $result")
+
+    // No tenary operator in Kotlin back can do this... and remove the curly braces since single statement
+    val resultTernary = if (number > 5) "Bigger than 5" else "Smaller than 5"
+    println("Result is that: $resultTernary")
 
     println("Which is bigger? 10 or 5:  " + bigger(10, 5))
     println("Which is smaller? 10 or 5:  " + smaller(10, 5))
