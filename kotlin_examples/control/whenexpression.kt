d04f5073464e7a30b0f11b7fadee3caf5cd9cd65
Jack hosking <jackhosking2@outlook.com>
2019-09-06 17:49:19 +1200

    type checking as well

--- kotlin_examples/control/whenexpression.kt
+++ kotlin_examples/control/whenexpression.kt
@@ -3,26 +3,40 @@ package kotlin_examples.control
 fun rangeCheck(x: Number) = when (x) {
     0 -> "Zero"
     in 1..4 -> "between one and four"
+    6, 9, 100 ->"6 nine or 100"
     else -> "Something else"
 }
 
 fun mathOperations(operator: String?, a: Int?, b: Int?) {
     when (operator) {
-        "+" -> println("$a + $b  = ${a?.plus(b!!)}")
-        "-" -> println("$a - $b  = ${a?.minus(b!!)}")
-        "/" -> println("$a / $b  = ${a?.div(b!!)}")
-        "*" -> println("$a * $b  = ${a?.times(b!!)}")
+        "+" -> "$a + $b  = ${a?.plus(b!!)}"
+        "-" -> "$a - $b  = ${a?.minus(b!!)}"
+        "/" -> "$a / $b  = ${a?.div(b!!)}"
+        "*" -> "$a * $b  = ${a?.times(b!!)}"
         else -> println("Not a valid operator")
+    }
+}
+
+fun typeCheck(type: Any) {
+    when(type) {
+        is Int -> println("Is int")
+        is String -> println("Is String")
+        is Boolean -> println("Is boolean")
     }
 }
 
 fun main(args: Array<String>) {
     println(rangeCheck(8))
 
+    println("Pick a math operator i.e. / + - *")
     val operator: String? = readLine()
+    println("Pick two numbers to be calculated")
     val a: String? = readLine()
     val b: String? = readLine()
 
-    mathOperations(operator, a?.toInt(), b?.toInt())
+    println(mathOperations(operator, a?.toInt(), b?.toInt()))
 
+    typeCheck("Hello")
+    typeCheck(1)
+    typeCheck(false)
 }
