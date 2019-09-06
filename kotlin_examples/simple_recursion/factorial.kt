82abc6ec9cab814cca8aba98bb9fabbad6bbd787
Jack hosking <jackhosking2@outlook.com>
2019-09-06 23:04:01 +1200

    tail recursive fibonacci sequence.

--- kotlin_examples/simple_recursion/factorial.kt
+++ kotlin_examples/simple_recursion/factorial.kt
@@ -1,14 +1,20 @@
 package kotlin_examples.simple_recursion
 
 fun factorial(n: Int): Long {
-    return if(n == 1) n.toLong() else n * factorial(n - 1 )
+    return if (n == 1) n.toLong() else n * factorial(n - 1)
 }
-fun main(args: Array<String>){
-    val number = 5
-    val result: Long
+
+tailrec fun fibonacci(n: Int, a: Long, b: Long): Long {
+    return if (n == 0) b else fibonacci(n - 1, a + b, a)
+}
+
+fun main(args: Array<String>) {
+    val number = 9
+    var result: Long
 
     result = factorial(number)
     println("The factorial of $number is: $result")
 
-
+    result = fibonacci(number, 0, 1)
+    println("The $number number of fibonacci is: $result")
 }
